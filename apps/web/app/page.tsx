702ccd5672149933e91ca972d50589d5b26dc8d6
Aditya Choudhary <adi854105@gmail.com>
2024-04-18 20:41:07 +0530

    first commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/button";
+
+export default function Page(): JSX.Element {
+  return (
+    <>Text to 3D app
+    <Button appName="text-to-3d">Click me</Button>
+    </>
+    
+  );
+}
