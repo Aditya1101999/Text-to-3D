64278ac94c7935ec09877c23c38c6d64b4c0f588
Aditya Choudhary <adi@Adityas-MacBook-Air-2.local>
2024-11-14 17:35:00 +0530

    final changes

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -8,7 +8,4 @@ app.use(express.json());
 
 app.use('/api/v1', generate3dRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+export default app;
